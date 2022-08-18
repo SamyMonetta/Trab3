06e25d42b5255a15d9459187b468d9ccb5d1cc86
SamyMonetta <samisami_monetta@hotmail.com>
2022-08-18 11:39:58 -0300

    .

--- Trab3/src/app/app.module.ts
+++ Trab3/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialesComponent } from './components/sociales/sociales.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    SocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
